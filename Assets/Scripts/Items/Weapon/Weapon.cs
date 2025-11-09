c2b9986887e26da8f6110654dda4d863f667871b
Maria <mariadybdahl@hotmail.dk>
2025-11-09 22:19:24 +0100

    lavet så den har påføres skade monster, og skrevet summary

--- Assets/Scripts/Items/Weapon/Weapon.cs
+++ Assets/Scripts/Items/Weapon/Weapon.cs
@@ -5,21 +5,20 @@ public abstract class Weapon : Item, IWeapon
     public abstract int CalculateDamage();
     public bool isAttacking = false;
 
-
-    //TODO Tilføje attack mod monster 
     /// <summary>
-    /// 
+    /// Håndterer, når våbnets trigger er i kontakt med et andet objekt.
+    /// Hvis våbnet er i gang med at angribe og objektet er et monster,
+    /// påføres monstret skade.
     /// </summary>
-    /// <param name="other"></param>
+    /// <param name="other">Den collider, der befinder sig inde i våbnets trigger.</param>
     private void OnTriggerStay(Collider other)
     {
         if (!isAttacking) return;
+        Monster monster = other.GetComponent<Monster>();
+        if (monster == null) return;
+        if (monster.CurrentHealth == 0) return;
 
-        //Under implementering når monster script er lavet
-        //if (==0) return; tjekker om det er fjende er død implemter når man har monster script og fjerne sætning
-        //if (monster != null) //Tjekker om monster er null
-        //  {
-        //     monster.TakeDamage(AttackDamage());
-        // }
+        monster.ReceiveDamage(CalculateDamage());
+        Debug.Log(name + " rammer " + monster.name + " for " + CalculateDamage() + " skade");
     }
 }    
---
+++ Assets/Scripts/Moster/Monster.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+    /// <summary>
+    /// Sætter CurrentHealth til MaxHealth, når objektet starter.
+    /// </summary>
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+    /// <summary>
+    /// Påfører dette monster skade og opdaterer dets nuværende liv.
+    /// Livet begrænses til området mellem 0 og MaxHealth.
+    /// </summary>
+    /// <param name="damageAmount">Mængden af skade, der skal påføres.</param>
+    public void ReceiveDamage(int damageAmount)
+    {
+        if (damageAmount < 0) return;
+
+        CurrentHealth -= damageAmount;
+
+        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
+
+        Debug.Log(" HP: " + CurrentHealth);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
