5b24cc43bba5869a8752e170adf92f379c43958a
Becky Berntsen Lau <150661633+Becky-B-Lau@users.noreply.github.com>
2025-12-04 13:11:37 +0100

    Fixy

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,19 +2,4 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public enum ItemType
-    {
-        Key,
-        Potion,
-        Weapon
-    }
-
-    [Tooltip("Category of this item")]
-    public ItemType itemType = ItemType.Potion;
-
-    [Tooltip("For Key/Weapon: index (0..2) selecting which variant this is. For Potion this is ignored.")]
-    public int variantId = 0;
-
-    [Tooltip("For stackable items like potions, how many to pick up")]
-    public int amount = 1;
 }
